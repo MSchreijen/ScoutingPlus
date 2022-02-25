d13cbb74f060e976805dcb0de31cb46460bcb3ea
MSchreijen <merijn.schreijen@gmail.com>
2022-02-25 09:56:34 +0100

    Spring Initializer

---
+++ src/main/java/com/its/scoutingplus/ScoutingPlusApplication.java
@@ -0,0 +1,13 @@
+package com.its.scoutingplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutingPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoutingPlusApplication.class, args);
+    }
+
+}
